cdf598745372f5bc339e20971140f7b73a0420ff
Thomas Kollbach <thomas@kollba.ch>
2022-10-29 16:08:06 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # solarman-widget-scriptable
-Widget for the Solarman API using scriptable.app
+
+Widget for the Solarman API using scriptable.app. The API has been reverse engineered from the [SOLARMAN iOS app](https://apps.apple.com/us/app/solarman-smart/id1469487897).
+
+## Dependencies
+
+To employ SHA256 the app uses the [sjcl](https://github.com/bitwiseshiftleft/sjcl) library. 
+
+## Usage
+
+1. Fill in `USERNAME` and `PASSWORD` on top of the `solarman-widget.js` file with your login data.
+2. Copy `solarman-widget.js` and `lib/sjcl.js` into your Scriptable directory (see [scriptable.app](https://scriptable.app) for more infos).
+3. Add a small widget to your home screen and choose solarman-widget in the widget config.
---
+++ solarman-widget.js
@@ -0,0 +1,151 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: magic;
+const sjcl = importModule("lib/sjcl");
+
+// License: See LICENSE file
+// Use with https://scriptable.app
+
+// -- Configuration --
+const API_HOST = "api4home.solarmanpv.com";
+const USERNAME = "email@example.com";
+const PASSWORD = "yourPassword";
+// -------------------
+
+function query(params) {
+  let result = [];
+  for (const key in params) {
+    if (Object.hasOwnProperty.call(params, key)) {
+      const value = params[key];
+      result.push(`${key}=${value}`);
+    }
+  }
+  return result.join("&");
+}
+
+async function getToken(username, password) {
+  const hash = sjcl.hash.sha256.hash(password);
+  const hashedPassword = sjcl.codec.hex.fromBits(hash);
+  console.log(`Hash PW ${hashedPassword}`);
+  const params = query({
+    system: "SOLARMAN",
+    grant_type: "password",
+    username: username,
+    password: hashedPassword,
+    client_id: "test",
+    clear_text_pwd: password,
+    identity_type: "2",
+  });
+  console.log(`Params: ${params}`);
+
+  const request = new Request(
+    `https://${API_HOST}/oauth-s/oauth/token?${params}`
+  );
+  request.method = "POST";
+  console.log(`Start request ${request}`);
+
+  request.body = params.toString();
+  const data = await request.loadJSON();
+  return data;
+}
+
+async function stationSearch(token, page = 1) {
+  const body = {
+    region: {
+      nationId: null,
+      level1: null,
+      level2: null,
+      level3: null,
+      level4: null,
+      level5: null,
+    },
+  };
+  const request = new Request(
+    `https://${API_HOST}/maintain-s/operating/station/search?page=${page}&size=10&order.direction=ASC&order.property=name`
+  );
+  request.method = "POST";
+  request.body = JSON.stringify(body);
+  request.headers = {
+    Authorization: `bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+
+  const data = await request.loadJSON();
+  return data;
+}
+
+function createWidget(homeData) {
+  const widget = new ListWidget();
+  const gradient = new LinearGradient();
+  gradient.colors = [new Color("#0064A6"), new Color("#D6A94B")];
+  gradient.locations = [0, 1];
+  widget.backgroundGradient = gradient;
+
+  const homeName = widget.addText(`☀️⚡️ ${homeData.name}`);
+  homeName.textColor = Color.white();
+  homeName.font = Font.boldSystemFont(12);
+
+  widget.addSpacer();
+
+  const wattText = `${homeData.currentWatts.toFixed(homeData.currentWatts > 100 ? 0 : 1) }W`
+  const titleText = widget.addText(wattText);
+  titleText.textColor = Color.white();
+  titleText.font = Font.boldSystemFont(32);
+
+  const totalkWh = `${homeData.generationValue.toFixed(1) }kWh today`
+  const totalText = widget.addText(totalkWh);
+  totalText.textColor = Color.white();
+  totalText.font = Font.subheadline();
+  
+  widget.addSpacer();
+  
+  const updatedAtText = widget.addText(homeData.lastUpdatedLabel);
+  updatedAtText.textColor = Color.white();
+  updatedAtText.font = Font.caption2();
+
+  
+
+  return widget;
+}
+
+function extractHomeData(home) {
+  const lastUpdated = new Date();
+  lastUpdated.setTime(home.lastUpdateTime * 1000.0);
+  const formatter = new DateFormatter();
+  if (new Date() - lastUpdated < 24 * 60 * 60 * 1000) {
+    formatter.useShortTimeStyle();
+    formatter.useNoDateStyle();
+  } else {
+    formatter.useNoTimeStyle();
+    formatter.useShortDateStyle();
+  }
+  
+  
+
+  return {
+    lastUpdated,
+    lastUpdatedLabel: `Updated ${formatter.string(lastUpdated, new Date())}`,
+    currentWatts: home.generationPower,
+    generationValue: home.generationValue,
+    percentOfCapacity: home.generationCapacity,
+    name: home.name,
+    temperature: home.temperature,
+    weather: home.weather,
+  };
+}
+
+const token = await getToken(USERNAME, PASSWORD);
+// console.log(`getToken Response: ${JSON.stringify(token)}`);
+const searchResponse = await stationSearch(token.access_token);
+// console.log(`searchResponse Response: ${JSON.stringify(searchResponse)}`);
+const [firstHome] = searchResponse.data;
+const homeData = extractHomeData(firstHome);
+console.log(`Home Response: ${JSON.stringify(homeData)}`);
+
+// if (config.runsInWidget) {
+let widget = createWidget(homeData);
+Script.setWidget(widget);
+Script.complete();
+// } else {
+//   console.log("Hello!")
+// }
